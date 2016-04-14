e986ebf1e232048c81153f957e4820770b7b016c
Yan Zalavin <yztrnc@gmail.com>
2016-04-14 18:16:33 +0300

    Tag machine field after initializing

--- public/game.js
+++ public/game.js
@@ -3,30 +3,34 @@ var colors = {
   machine: '#008fb3'
 }
 
+var tengen = "td#h8";
+var tag = 'tagged';
+
 function tagField(field, color){
   $(field).css('background', color);
-  $(field).addClass('tagged');
+  $(field).addClass(tag);
 }
 
 $(function(){
-  tagField('td#h8', colors.human);
+  tagField(tengen, colors.human);
+  tagField('td#h9', colors.machine);
   $("td").on('click', function(){
-    if ($(this).hasClass('tagged')){
-      alert('Не стоит этого делать');
-    } else {
-      tagField(this, colors.human)
-      $.post('step', {el: $(this).attr('id')}, function(data){
-        console.log(data);
-        if (data == 'win') {
-          alert('Это победа!');
-        } else if (data == 'loose'){
-          alert('Неудача!');
-        } else if (data == 'draw'){
-          alert('Что ж! Победитель не выявлен.')
-        } else {
-          tagField($('td#' + data), colors.machine);
-        }
-      })
-    }
-  })
+      if ($(this).hasClass('tagged')){
+        alert('Не стоит этого делать');
+      } else {
+        tagField(this, colors.human)
+        $.post('step', {el: $(this).attr('id')}, function(data){
+          console.log(data);
+          if (data == 'win') {
+            alert('Это победа!');
+          } else if (data == 'loose'){
+            alert('Неудача!');
+          } else if (data == 'draw'){
+            alert('Что ж! Победитель не выявлен.')
+          } else {
+            tagField($('td#' + data), colors.machine);
+          }
+        })
+      }
+    })
 })
